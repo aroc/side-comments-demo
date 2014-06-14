c4501c384940f2230441ae52716f2af2d98e50a7
Eric Anderson <eric@ericanderson.ca>
2014-06-14 16:49:18 -0600

    Auto-add comments in the callback.

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -20092,12 +20092,20 @@ require.alias("side-comments-demo/main.js", "side-comments-demo/index.js");
 var SideComments = require('side-comments');
 
 $(document).ready(function(){
-  var sideComments;
+
+  function initSideComments( existingComments ) {
+    sideComments = new SideComments('#commentable-container', existingComments);
+    sideComments.on('commentPosted', function( comment ) {
+      comment.id = parseInt(Math.random() * (1000000 - 1) + 1);
+      sideComments.insertComment(comment);
+    });
+  }
+
   $.ajax({
     url: 'http://localhost:3000/comments',
     type: 'GET',
     success: function( results ) {
-      sideComments = new SideComments('#commentable-container', results);
+      initSideComments(results);
     },
     error: function( xhr, status, error ) {
       console.log('Oh noes! An error!');
