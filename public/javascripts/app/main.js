53669536195635907e77429cb9a836d8f69f3fe8
Eric Anderson <eric@ericanderson.ca>
2014-06-14 17:15:03 -0600

    Including some basic characters for the side comments.

--- public/javascripts/app/main.js
+++ public/javascripts/app/main.js
@@ -2,23 +2,43 @@ var SideComments = require('side-comments');
 
 $(document).ready(function(){
 
+  function getComments( existingComments ) {
+    $.ajax({
+      url: 'http://localhost:3000/comments',
+      type: 'GET',
+      success: function( results ) {
+        initSideComments(results);
+      },
+      error: function( xhr, status, error ) {
+        console.log('Oh noes! An error!');
+      }
+    });
+  }
+
   function initSideComments( existingComments ) {
-    sideComments = new SideComments('#commentable-container', existingComments);
+    sideComments = new SideComments(
+      '#commentable-container',
+      {
+        id: githubUser.id,
+        avatarUrl: githubUser.avatar_url,
+        name: githubUser.name
+      },
+      existingComments
+    );
     sideComments.on('commentPosted', function( comment ) {
       comment.id = parseInt(Math.random() * (1000000 - 1) + 1);
       sideComments.insertComment(comment);
     });
   }
 
-  $.ajax({
-    url: 'http://localhost:3000/comments',
-    type: 'GET',
-    success: function( results ) {
-      initSideComments(results);
-    },
-    error: function( xhr, status, error ) {
-      console.log('Oh noes! An error!');
+  var githubUser = new Gh3.User("aroc");
+  githubUser.fetch(function( err, res ) {
+    if (err) {
+      // TODO: Throw an error, "Couldn't find a user with that username. Try again."
+    } else {
+      getComments();
     }
   });
+
 
 });
--- routes/index.js
+++ routes/index.js
@@ -13,14 +13,14 @@ router.get('/comments', function(req, res) {
       "sectionId": "1",
       "comments": [
         {
-          "authorAvatarUrl": "https://d262ilb51hltx0.cloudfront.net/fit/c/64/64/0*bBRLkZqOcffcRwKl.jpeg",
-          "authorName": "Eric Anderson",
-          "comment": "Hey there!"
+          "authorAvatarUrl": "http://f.cl.ly/items/1W303Y360b260u3v1P0T/jon_snow_small.png",
+          "authorName": "Jon Sno",
+          "comment": "I'm Ned Stark's bastard. Related: I know nothing."
         },
         {
-          "authorAvatarUrl": "https://d262ilb51hltx0.cloudfront.net/fit/c/64/64/0*bBRLkZqOcffcRwKl.jpeg",
-          "authorName": "Jim Beam",
-          "comment": "I'm drunk!"
+          "authorAvatarUrl": "http://f.cl.ly/items/2o1a3d2f051L0V0q1p19/donald_draper.png",
+          "authorName": "Donald Draper",
+          "comment": "I need a scotch."
         }
       ]
     },
@@ -28,9 +28,9 @@ router.get('/comments', function(req, res) {
       "sectionId": "3",
       "comments": [
         {
-          "authorAvatarUrl": "https://d262ilb51hltx0.cloudfront.net/fit/c/64/64/0*bBRLkZqOcffcRwKl.jpeg",
-          "authorName": "Jim Beam",
-          "comment": "I'm drunk!"
+          "authorAvatarUrl": "http://f.cl.ly/items/0l1j230k080S0N1P0M3e/clay-davis.png",
+          "authorName": "Senator Clay Davis",
+          "comment": "These Side Comments are incredible. Sssshhhiiiiieeeee."
         }
       ]
     }
