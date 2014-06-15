b6b9d08c8c5a30e4da64893d02f8377aeb30cad4
Eric Anderson <eric@ericanderson.ca>
2014-06-15 14:31:05 -0600

    Work on getting started and docs.

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -21108,26 +21108,13 @@ var SideComments = require('side-comments');
 
 $(document).ready(function(){
 
-  function getComments( existingComments ) {
-    $.ajax({
-      url: 'http://localhost:3000/comments',
-      type: 'GET',
-      success: function( results ) {
-        initSideComments(results);
-      },
-      error: function( xhr, status, error ) {
-        console.log('Oh noes! An error!');
-      }
-    });
-  }
-
   function initSideComments( existingComments ) {
     sideComments = new SideComments(
       '#commentable-container',
       {
-        id: githubUser.id,
-        avatarUrl: githubUser.avatar_url,
-        name: githubUser.name
+        id: 1,
+        avatarUrl: "http://f.cl.ly/items/0s1a0q1y2Z2k2I193k1y/default-user.png",
+        name: "You"
       },
       existingComments
     );
@@ -21137,14 +21124,15 @@ $(document).ready(function(){
     });
   }
 
-  var githubUser = new Gh3.User("aroc");
-  githubUser.fetch(function( err, res ) {
-    if (err) {
-      // TODO: Throw an error, "Couldn't find a user with that username. Try again."
-    } else {
-      getComments();
+  $.ajax({
+    url: 'http://localhost:3000/comments',
+    type: 'GET',
+    success: function( results ) {
+      initSideComments(results);
+    },
+    error: function( xhr, status, error ) {
+      console.log('Oh noes! An error!');
     }
   });
-
 
 });
