899d40a70064e7a8e1e1900853ed586eb5839a6a
Eric Anderson <eric@ericanderson.ca>
2014-06-21 23:23:03 -0600

    Updating SideComments.

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -10677,7 +10677,6 @@ Emitter.prototype.hasListeners = function(event){
 require.register("aroc-side-comments/js/main.js", function(exports, require, module){
 var _ = require('./vendor/lodash-custom.js');
 var Section = require('./section.js');
-var mobilecheck = require('./helpers/mobile-check.js');
 var Emitter = require('emitter');
 var eventPipe = new Emitter;
 
@@ -10698,7 +10697,6 @@ function SideComments( el, currentUser, existingComments ) {
   this.$el = $(el);
   this.$body = $('body');
   this.eventPipe = eventPipe;
-  var clickEvent = (mobilecheck()) ? "touchstart" : "click";
 
   this.currentUser = _.clone(currentUser) || null;
   this.existingComments = _.cloneDeep(existingComments) || [];
@@ -10713,7 +10711,7 @@ function SideComments( el, currentUser, existingComments ) {
   this.eventPipe.on('commentPosted', _.bind(this.commentPosted, this));
   this.eventPipe.on('commentDeleted', _.bind(this.commentDeleted, this));
   this.eventPipe.on('addCommentAttempted', _.bind(this.addCommentAttempted, this));
-  this.$body.on(clickEvent, _.bind(this.bodyClick, this));
+  this.$body.on('click', _.bind(this.bodyClick, this));
 
   this.initialize(this.existingComments);
 }
@@ -10892,7 +10890,6 @@ module.exports = SideComments;
 });
 require.register("aroc-side-comments/js/section.js", function(exports, require, module){
 var _ = require('./vendor/lodash-custom.js');
-var mobilecheck = require('./helpers/mobile-check.js');
 var Template = require('../templates/section.html');
 var CommentTemplate = require('../templates/comment.html');
 
@@ -10907,15 +10904,14 @@ function Section( eventPipe, $el, currentUser, comments ) {
 	this.$el = $el;
 	this.comments = comments ? comments.comments : [];
 	this.currentUser = currentUser || null;
-	var clickEvent = (mobilecheck()) ? "touchstart" : "click";
 	
 	this.id = $el.data('section-id');
 
-	this.$el.on(clickEvent, '.side-comment .marker', _.bind(this.markerClick, this));
-	this.$el.on(clickEvent, '.side-comment .add-comment', _.bind(this.addCommentClick, this));
-	this.$el.on(clickEvent, '.side-comment .post', _.bind(this.postCommentClick, this));
-	this.$el.on(clickEvent, '.side-comment .cancel', _.bind(this.cancelCommentClick, this));
-	this.$el.on(clickEvent, '.side-comment .delete', _.bind(this.deleteCommentClick, this));
+	this.$el.on('click', '.side-comment .marker', _.bind(this.markerClick, this));
+	this.$el.on('click', '.side-comment .add-comment', _.bind(this.addCommentClick, this));
+	this.$el.on('click', '.side-comment .post', _.bind(this.postCommentClick, this));
+	this.$el.on('click', '.side-comment .cancel', _.bind(this.cancelCommentClick, this));
+	this.$el.on('click', '.side-comment .delete', _.bind(this.deleteCommentClick, this));
 
 	this.render();
 }
@@ -13541,13 +13537,6 @@ require.register("aroc-side-comments/js/vendor/lodash-custom.js", function(expor
 }.call(this));
 
 });
-require.register("aroc-side-comments/js/helpers/mobile-check.js", function(exports, require, module){
-module.exports = function() {
-	var check = false;
-	(function(a){if(/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows (ce|phone)|xda|xiino/i.test(a)||/1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(a.substr(0,4)))check = true})(navigator.userAgent||navigator.vendor||window.opera);
-	return check;
-}
-});
 
 require.register("aroc-side-comments/templates/section.html", function(exports, require, module){
 module.exports = '<div class="side-comment <%= sectionClasses %>">\n  <a href="#" class="marker">\n    <span><%= comments.length %></span>\n  </a>\n  \n  <div class="comments-wrapper">\n    <ul class="comments">\n      <% _.each(comments, function( comment ){ %>\n        <%= _.template(commentTemplate, { comment: comment, currentUser: currentUser }) %>\n      <% }) %>\n    </ul>\n    \n    <a href="#" class="add-comment">Leave a comment</a>\n    \n    <% if (currentUser){ %>\n      <div class="comment-form">\n        <div class="author-avatar">\n          <img src="<%= currentUser.avatarUrl %>">\n        </div>\n        <p class="author-name">\n          <%= currentUser.name %>\n        </p>\n        <div class="comment-box" contenteditable="true" data-placeholder-content="Leave a comment..."></div>\n        <div class="actions">\n          <a href="#" class="action-link post">Post</a>\n          <a href="#" class="action-link cancel">Cancel</a>\n        </div>\n      </div>\n    <% } %>\n  </div>\n</div>';
@@ -13558,21 +13547,11 @@ module.exports = '<li data-comment-id="<%= comment.id %>">\n  <div class="author
 require.alias("aroc-side-comments/js/main.js", "side-comments-demo/deps/side-comments/js/main.js");
 require.alias("aroc-side-comments/js/section.js", "side-comments-demo/deps/side-comments/js/section.js");
 require.alias("aroc-side-comments/js/vendor/lodash-custom.js", "side-comments-demo/deps/side-comments/js/vendor/lodash-custom.js");
-require.alias("aroc-side-comments/js/helpers/mobile-check.js", "side-comments-demo/deps/side-comments/js/helpers/mobile-check.js");
 require.alias("aroc-side-comments/js/main.js", "side-comments-demo/deps/side-comments/index.js");
 require.alias("aroc-side-comments/js/main.js", "side-comments/index.js");
 require.alias("component-emitter/index.js", "aroc-side-comments/deps/emitter/index.js");
 
 require.alias("aroc-side-comments/js/main.js", "aroc-side-comments/index.js");
-function mobilecheck() {
-  var check = false;
-  (function(a){if(/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows (ce|phone)|xda|xiino/i.test(a)||/1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(a.substr(0,4)))check = true})(navigator.userAgent||navigator.vendor||window.opera);
-  return check;
-}
-
-var isMobile = mobilecheck();
-var clickEvent = (mobilecheck()) ? "touchstart" : "click";
-
 var existingComments = [
   {
     "sectionId": "1",
@@ -13640,12 +13619,12 @@ $(document).ready(function(){
     }
   });
 
-  $('#mobile-menu-btn').on(clickEvent, function( event ) {
+  $('#mobile-menu-btn').on('click', function( event ) {
     event.preventDefault();
     $('body').addClass('show-mobile');
   });
 
-  $(document).on(clickEvent, '.show-mobile #nav ul a, .show-mobile #nav .logo, #close-nav-overlay', function( event ) {
+  $(document).on('click', '.show-mobile #nav ul a, .show-mobile #nav .logo, #close-nav-overlay', function( event ) {
     var $target = $(event.target);
     if ($target.attr('id') == 'close-nav-overlay') {
       event.preventDefault();
